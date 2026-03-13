aefaba89aaca55d0dc8cdc9fbf791a6f4af87dcf
tchrapovic <chrapovic.tadeas@gmail.com>
2026-03-13 13:34:31 +0100

    Smart integration: handle not changed situation in mapping simulation pages

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/page/admin/simulation/panel/mapping/changes/SimulationChangesPanel.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/page/admin/simulation/panel/mapping/changes/SimulationChangesPanel.java
@@ -5,37 +5,55 @@
  */
 package com.evolveum.midpoint.gui.impl.page.admin.simulation.panel.mapping.changes;
 
-import com.evolveum.midpoint.gui.api.component.BasePanel;
+import java.util.List;
 
+import com.evolveum.midpoint.gui.api.component.BasePanel;
 import com.evolveum.midpoint.gui.impl.page.admin.simulation.panel.mapping.changes.model.SimulationChangeSummaryDto;
 
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.model.IModel;
-
-import java.util.List;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Panel displaying a list of summarized mapping simulation changes.
  */
 public class SimulationChangesPanel extends BasePanel<List<SimulationChangeSummaryDto>> {
 
+    private static final String ID_EMPTY = "empty";
     private static final String ID_ROWS = "rows";
     private static final String ID_ROW = "row";
 
     public SimulationChangesPanel(String id, IModel<List<SimulationChangeSummaryDto>> model) {
         super(id, model);
+    }
+
+    @Override
+    protected void onInitialize() {
+        super.onInitialize();
         initLayout();
     }
 
     private void initLayout() {
         setOutputMarkupId(true);
 
-        add(new ListView<>(ID_ROWS, getModel()) {
+        List<SimulationChangeSummaryDto> changes = getModelObject();
+        boolean hasChanges = changes != null && !changes.isEmpty();
+
+        Label emptyLabel = new Label(ID_EMPTY, createStringResource("SimulationChangesPanel.noChanges"));
+        emptyLabel.setVisible(!hasChanges);
+        emptyLabel.setOutputMarkupPlaceholderTag(true);
+        add(emptyLabel);
+
+        ListView<SimulationChangeSummaryDto> rows = new ListView<>(ID_ROWS, getModel()) {
             @Override
-            protected void populateItem(ListItem<SimulationChangeSummaryDto> item) {
+            protected void populateItem(@NotNull ListItem<SimulationChangeSummaryDto> item) {
                 item.add(new SimulationChangeValuesPanel(ID_ROW, item.getModel()));
             }
-        });
+        };
+        rows.setVisible(hasChanges);
+        rows.setOutputMarkupPlaceholderTag(true);
+        add(rows);
     }
 }
